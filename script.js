12eb6e3a64a549b40877b5c92fe6d34a25285d41
epitiffany <tiffanylam38@yahoo.com>
2020-10-24 20:52:06 -0500

    created alert boxes for categories

---
+++ script.js
@@ -0,0 +1,52 @@
+// C WEBPAGE
+function c_var(){
+  alert('edit c_var function in script.js ');
+}
+
+function c_br(){
+  alert('edit c_br function in script.js');
+}
+
+function c_arr(){
+  alert('edit c_arr function in script.js');
+}
+
+function c_func(){
+  alert('edit c_func function in script.js');
+}
+
+
+// ASSEMBLY WEBPAGE
+function a_bits(){
+  alert('edit a_bits function in script.js');
+}
+
+function a_lc3(){
+  alert('edit a_lc3 function in script.js');
+}
+
+function a_instruct(){
+  alert('edit a_instruct function in script.js');
+}
+
+function a_data(){
+  alert('edit a_data function in script.js');
+}
+
+
+// JAVA WEBPAGE
+function j_var(){
+  alert('edit j_var function in script.js');
+}
+
+function j_class(){
+  alert('edit j_class function in script.js');
+}
+
+function j_condit(){
+  alert('edit j_condit function in script.js');
+}
+
+function j_arr(){
+  alert('edit j_arr function in script.js');
+}
